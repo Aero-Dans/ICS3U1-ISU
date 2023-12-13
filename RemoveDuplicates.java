0ba92250ec352291717fba7c90012f5e900aa13e
Aero-Dans <152432346+Aero-Dans@users.noreply.github.com>
2023-12-13 14:27:15 -0500

    Update RemoveDuplicates.java
    Changed the class name

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Main {
+class RemoveDuplicates {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     ArrayList<Integer> database = new ArrayList<Integer>();
